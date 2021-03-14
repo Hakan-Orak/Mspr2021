bacc9f119edeeb54cc094ea8b9351f7cb4ccec70
nolimitestar59 <hakan.orak@hotmail.fr>
2021-03-14 17:17:06 +0100

    partie de hakan accueil bg

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,30 @@
 <template>
-  <q-page class="row items-center justify-evenly">
+  <div class="">
+    <q-parallax>
+      <template  style="height: 100%; width: 100%" v-slot:media>
+        <img src="../../public/bg/background-accueil.png">
+      </template>
 
-    <qrcode-stream @decode="onDecode"></qrcode-stream>
+      <template v-slot:content="scope">
+        <div
+          class="absolute column items-center"
+          :style="{
+            // opacity: 0.45 + (1 - scope.percentScrolled) * 0.55,
+            top: (scope.percentScrolled * 60) + '%',
+            left: 0,
+            right: 0
+          }"
+        >
 
-  </q-page>
+<!--          <div class="text-h3 text-white text-center">Scanner vos QR CODE</div>-->
+          <img @click="$router.push('/qrCodeReader')" src="../../public/bg/scan-qrCode.png" style="width: 150px; height: 150px">
+
+          <img @click="$router.push('/listePromotion')" src="../../public/bg/valise-stock.png" style="width: 150px; height: 150px">
+
+        </div>
+      </template>
+    </q-parallax>
+  </div>
 </template>
 
 <script>
@@ -22,6 +43,8 @@ export default {
   },
   data() {
     return {
+      email:'',
+      password : '',
     salut : "salut hakan",
       allQrCode : [],
 
